6990751459d74a8303256fb7ae164862d0fbedbc
merobal <facebook@merobal.hu>
2017-05-17 14:57:07 +0200

    Add datepicker

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import { MaterialModule, NoConflictStyleCompatibilityMode, MdNativeDateModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
 
@@ -11,8 +11,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule,
-    HttpModule
+    BrowserAnimationsModule,
+    MaterialModule,
+    NoConflictStyleCompatibilityMode,
+    MdNativeDateModule
   ],
   providers: [],
   bootstrap: [AppComponent]
